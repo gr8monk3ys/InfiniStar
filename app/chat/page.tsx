0f4645d2bed2daf509402387a402475dbec86d0d
gr8monk3ys <lorenzosca7@protonmail.ch>
2023-06-08 22:48:27 -0700

    Initialized repo

---
+++ app/chat/page.tsx
@@ -0,0 +1,38 @@
+'use client'
+import { useState } from "react";
+
+import { Textarea } from "@/components/ui/textarea"
+import { Button } from "@/components/ui/button"
+
+export default function IndexPage() {
+  const [chat, setChat] = useState([
+    { user: 'bot', message: 'Hello, how can I assist you?' },
+    { user: 'user', message: 'Hi, I have a question about your service.' },
+    // add more chat messages here
+  ]);
+
+  return (
+    <section className="flex flex-col h-screen p-4 bg-gray-100">
+      <div className="mb-4">
+        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+          Chat <br className="hidden sm:inline" />
+        </h1>
+      </div>
+
+      <div className="flex-1 overflow-y-auto bg-white rounded-lg shadow p-4 space-y-2">
+        {chat.map((msg, index) => (
+          <div key={index} className={`flex items-start ${msg.user === 'user' ? 'justify-end' : ''}`}>
+            <div className={`rounded-lg px-3 py-2 ${msg.user === 'user' ? 'bg-blue-500 text-white' : 'bg-gray-300 text-black'}`}>
+              {msg.message}
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className="grid w-full gap-2 sticky bottom-0 py-2 ">
+      <Textarea placeholder="Type your message here." />
+      <Button>Send message</Button>
+      </div>
+    </section>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link"
+
+import { siteConfig } from "@/config/site"
+import { buttonVariants } from "@/components/ui/button"
+
+export default function IndexPage() {
+  return (
+    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+      <div className="flex max-w-[980px] flex-col items-start gap-2">
+        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+          Welcome to Infinistar <br className="hidden sm:inline" />
+          Interactive Virtual Chats with Advanced Language Learning Models
+        </h1>
+        <p className="max-w-[700px] text-lg text-muted-foreground">
+          Step into the future of language learning with Infinistar. Our
+          advanced AI technology creates virtual chats that feel just like texting someone 
+          you're interested in, making your learning experience interactive and enjoyable. 
+          Harness the power of AI for language learning, brought to you by Infinistar.
+        </p>
+      </div>
+
+      <div className="flex gap-4">
+        <Link
+          href={siteConfig.links.docs}
+          target="_blank"
+          rel="noreferrer"
+          className={buttonVariants()}
+        >
+          Documentation
+        </Link>
+        <Link
+          target="_blank"
+          rel="noreferrer"
+          href={siteConfig.links.github}
+          className={buttonVariants({ variant: "outline" })}
+        >
+          GitHub
+        </Link>
+      </div>
+      
+    </section>
+  )
+}
