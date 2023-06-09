96033b4922541b1fdee2b745b6743f8bba276da0
gr8monk3ys <lorenzosca7@protonmail.ch>
2023-06-09 14:36:12 -0700

    More boilerplate text for now

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,12 @@
-import Link from "next/link"
-
-import { siteConfig } from "@/config/site"
-import { buttonVariants } from "@/components/ui/button"
-
 export default function IndexPage() {
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <div className="flex max-w-[980px] flex-col items-start gap-2">
+      <div className="flex max-w-[80%] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
           Welcome to Infinistar <br className="hidden sm:inline" />
         </h1>
         <p className="max-w-[700px] text-lg text-muted-foreground">
-          Interactive Virtual Chats with Advanced Language Learning Models Step
+          Interactive Virtual Chats with Advanced Language Learning Models. Step
           into the future of language learning with Infinistar. Our advanced AI
           technology creates virtual chats that feel just like texting someone
           you are interested in, making your learning experience interactive and
@@ -19,6 +14,40 @@ export default function IndexPage() {
           you by Infinistar.
         </p>
       </div>
+        <section className="mt-10">
+          <h2 className="text-2xl font-extrabold">Developer Experience</h2>
+          <p className="max-w-[700px] text-lg text-muted-foreground">
+            At Infinistar, we value our developers. Our APIs are designed to be robust, reliable and easy to use, making your development process smooth and efficient.
+          </p>
+        </section>
+
+        <section className="mt-10">
+          <h2 className="text-2xl font-extrabold">Community</h2>
+          <p className="max-w-[700px] text-lg text-muted-foreground">
+            Join a thriving community of developers and learners. Share your experiences, learn from others, and contribute to the growth of the platform.
+          </p>
+        </section>
+
+        <section className="mt-10">
+          <h2 className="text-2xl font-extrabold">What Can You Do with Infinistar?</h2>
+          <p className="max-w-[700px] text-lg text-muted-foreground">
+            From creating personalized language learning chatbots to developing advanced AI-driven language learning applications, the possibilities with Infinistar are endless.
+          </p>
+        </section>
+
+        <section className="mt-10">
+          <h2 className="text-2xl font-extrabold">Integrations</h2>
+          <p className="max-w-[700px] text-lg text-muted-foreground">
+            Infinistar integrates seamlessly with popular platforms and technologies, making it easier for you to incorporate our services into your applications.
+          </p>
+        </section>
+
+        <section className="mt-10">
+          <h2 className="text-2xl font-extrabold">Learn and Get Inspired</h2>
+          <p className="max-w-[700px] text-lg text-muted-foreground">
+            Explore our resources to learn more about AI and language learning, and see how others are using Infinistar to innovate in the field of language learning.
+          </p>
+        </section>
     </section>
   )
 }
