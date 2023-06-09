bf078c36542c7010058b8a7031da781f39719419
gr8monk3ys <lorenzosca7@protonmail.ch>
2023-06-09 14:48:00 -0700

    Added profile pic in corner

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -6,6 +6,7 @@ import { CommandMenu } from "@/components/command-menu"
 import { Icons } from "@/components/icons"
 import { MainNav } from "@/components/main-nav"
 import { ThemeToggle } from "@/components/theme-toggle"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
 export function SiteHeader() {
   return (
@@ -13,8 +14,8 @@ export function SiteHeader() {
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <MainNav items={siteConfig.mainNav} />
         <div className="flex flex-1 items-center justify-end space-x-4">
+        <CommandMenu />
           <nav className="flex items-center space-x-1">
-            <CommandMenu />
             <Link
               href={siteConfig.links.github}
               target="_blank"
@@ -30,22 +31,11 @@ export function SiteHeader() {
                 <span className="sr-only">GitHub</span>
               </div>
             </Link>
-            <Link
-              href={siteConfig.links.twitter}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div
-                className={buttonVariants({
-                  size: "sm",
-                  variant: "ghost",
-                })}
-              >
-                <Icons.twitter className="h-5 w-5 fill-current" />
-                <span className="sr-only">Twitter</span>
-              </div>
-            </Link>
             <ThemeToggle />
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
           </nav>
         </div>
       </div>
