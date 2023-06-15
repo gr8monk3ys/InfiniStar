58fa2e2f611852ba50beb8b1c3c712632cddfc44
gr8monk3ys <lorenzosca7@protonmail.ch>
2023-06-15 12:29:55 -0700

    Categorized main pages

---
+++ app/(marketing)/explore/page.tsx
@@ -0,0 +1,56 @@
+"use client"
+
+import Image from "next/image"
+import Link from "next/link"
+
+import { AspectRatio } from "@/components/ui/aspect-ratio"
+
+const characters = [
+  {
+    name: "Waifu 1",
+    image: "/../public/images/waifu1.png",
+    chatLink: "/chat/waifu1",
+    width: 500,
+    height: 500,
+  },
+  {
+    name: "Waifu 2",
+    image: "/../public/images/waifu2.png",
+    chatLink: "/chat/waifu2",
+    width: 500,
+    height: 500,
+  },
+  // ...
+]
+
+export default function ExplorePage() {
+  return (
+    <section className="flex h-screen flex-col p-4">
+      <div className="mb-4">
+        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+          Explore <br className="hidden sm:inline" />
+        </h1>
+      </div>
+      <div className="grid grid-cols-5 gap-4 p-4">
+        {characters.map((character) => (
+          <Link key={character.name} href={character.chatLink} passHref>
+            <div className="group relative cursor-pointer overflow-hidden rounded-lg">
+              <AspectRatio>
+                <Image
+                  src={character.image}
+                  alt={character.name}
+                  width={character.width}
+                  height={character.height}
+                  className="h-full w-full rounded-lg object-cover"
+                />
+              </AspectRatio>
+              <div className="absolute inset-0 flex items-center justify-center rounded-lg bg-black bg-opacity-10 transition duration-200 group-hover:bg-opacity-25">
+                <span className="font-bold text-white">{character.name}</span>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </section>
+  )
+}
---
+++ app/(marketing)/pricing/page.tsx
@@ -0,0 +1,63 @@
+import Link from "next/link"
+
+import { cn } from "@/lib/utils"
+import { buttonVariants } from "@/components/ui/button"
+import { Icons } from "@/components/icons"
+
+export const metadata = {
+  title: "Pricing",
+}
+
+export default function PricingPage() {
+  return (
+    <section className="container flex flex-col  gap-6 py-8 md:max-w-[64rem] md:py-12 lg:py-24">
+      <div className="mx-auto flex w-full flex-col gap-4 md:max-w-[58rem]">
+        <h2 className="font-heading text-3xl leading-[1.1] sm:text-3xl md:text-6xl">
+          Simple, and transparent
+        </h2>
+        <p className="max-w-[85%] leading-normal text-muted-foreground sm:text-lg sm:leading-7">
+          Unlock all features including unlimited posts for your blog.
+        </p>
+      </div>
+      <div className="grid w-full items-start gap-10 rounded-lg border p-10 md:grid-cols-[1fr_200px]">
+        <div className="grid gap-6">
+          <h3 className="text-xl font-bold sm:text-2xl">
+            What&apos;s included in the PRO plan
+          </h3>
+          <ul className="grid gap-3 text-sm text-muted-foreground sm:grid-cols-2">
+            <li className="flex items-center">
+              Unlimited Posts
+            </li>
+            <li className="flex items-center">
+              Unlimited Users
+            </li>
+
+            <li className="flex items-center">
+              Custom domain
+            </li>
+            <li className="flex items-center">
+              Dashboard Analytics
+            </li>
+            <li className="flex items-center">
+              Access to Discord
+            </li>
+            <li className="flex items-center">
+              Premium Support
+            </li>
+          </ul>
+        </div>
+        <div className="flex flex-col gap-4 text-center">
+          <div>
+            <h4 className="text-7xl font-bold">$15</h4>
+            <p className="text-sm font-medium text-muted-foreground">
+              Billed Monthly
+            </p>
+          </div>
+          <Link href="/login" className={cn(buttonVariants({ size: "lg" }))}>
+            Get Started
+          </Link>
+        </div>
+      </div>
+    </section>
+  )
+}
