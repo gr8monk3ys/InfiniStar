b1cbb806911f8ca2fcdfe3195a1d76c027a353af
gr8monk3ys <lorenzosca7@protonmail.ch>
2023-06-15 14:19:02 -0700

    Implemented prisma into chat api route

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,27 +1,23 @@
-import weaviate from "weaviate-ts-client"
+import { PrismaClient } from '@prisma/client';
+import type { NextApiRequest, NextApiResponse } from 'next'
 
-const client = weaviate.client({
-  scheme: "https",
-  host: "your-weaviate-instance-host",
-  apiKey: new weaviate.ApiKey("YOUR-WEAVIATE-API-KEY"),
-})
+const prisma = new PrismaClient();
 
-export default async function handler(req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
-    const { message } = req.body
+    const { message } = req.body;
 
-    // Save the message to Weaviate.
-    await client.c11n.objects.create({
-      class: "ChatMessage",
-      properties: {
+    // Save the message to the database using Prisma.
+    await prisma.chatMessage.create({
+      data: {
         user: "user",
         message: message,
       },
-    })
+    });
 
     // Return the saved message.
-    res.status(200).json({ message })
+    res.status(200).json({ message });
   } else {
-    res.status(405).json({ message: "Method not allowed." })
+    res.status(405).json({ message: "Method not allowed." });
   }
 }
